4f57d36207cc38f854323873571bfdd5941fa82a
buladou <kellyhu112@qq.com>
2017-02-27 22:11:26 +0800

    push

--- movie_spider/movie_spider/spiders/comedy.py
+++ movie_spider/movie_spider/spiders/comedy.py
@@ -7,11 +7,13 @@ class movie87_spider(scrapy.Spider):
     allowed_domains = ["www.87movie.com"]
     start_urls = ["http://www.87movie.com/tag/%E5%96%9C%E5%89%A7/"]
 
+    def parse_page(self, response):
+        pass
+
     def parse(self,response):
         num_page = response.xpath('//ul[@class="pagination"]//li[last()]/a/@href').extract()
         number = 1
         if len(num_page) > 0:
             number = int(num_page[0].split('/')[-1].split('?')[0])
         for i in range(1, number+1):
-            print(response.url + str(i) + '?o=data')
-            # yield scrapy.Request(response.url +'/'+ str(i) + '?o=data', meta={'tag': tag}, callback=self.parse_page)
+            yield scrapy.Request(response.url + str(i) + '?o=data', callback=self.parse_page)
