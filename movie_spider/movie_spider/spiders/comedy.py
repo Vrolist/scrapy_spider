4cd9fdf7efb31a5daede85ee20334d3624b88214
buladou <kellyhu112@qq.com>
2017-02-27 21:05:17 +0800

    push

--- movie_spider/movie_spider/spiders/comedy.py
+++ movie_spider/movie_spider/spiders/comedy.py
@@ -12,7 +12,6 @@ class movie87_spider(scrapy.Spider):
         number = 1
         if len(num_page) > 0:
             number = int(num_page[0].split('/')[-1].split('?')[0])
-        print(number)
         for i in range(1, number+1):
             print(response.url + str(i) + '?o=data')
             # yield scrapy.Request(response.url +'/'+ str(i) + '?o=data', meta={'tag': tag}, callback=self.parse_page)
