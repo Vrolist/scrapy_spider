90ff3389fc1949315226952011d4c16e3288f133
buladou <kellyhu112@qq.com>
2017-03-01 20:15:35 +0800

    push

--- movie_spider/movie_spider/spiders/db_movie.py
+++ movie_spider/movie_spider/spiders/db_movie.py
@@ -1,9 +1,21 @@
 import scrapy
+from ..items import MovieSpiderItem
 
 class db_movie(scrapy.Spider):
     name = "douban_movie"
     allowed_domains = ["movie.douban.com"]
     start_urls = ["https://movie.douban.com/later/beijing/"]
 
+    def parse_info(self, response):
+        movie_info = MovieSpiderItem()
+        movie_info['title'] = response.xpath("//div[@id='content']/h1/span/text()").extract()
+        movie_info['pic'] = response.xpath("//div[@id='mainpic']/a/img/@src").extract()
+        movie_info['content'] = response.xpath("//div[@id='link-report']/span/text()").extract()
+        movie_info['download'] = ""
+        return movie_info
+
     def parse(self, response):
-        print(response)
+        movie_links = response.xpath("//div[@id='showing-soon']/div/div/h3/a/@href").extract()
+        for i in movie_links:
+            print(i)
+            # yield scrapy.Request(i, callback=parse_info)
