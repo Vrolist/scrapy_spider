08e6b66df8ce5bcd157453c944e36072a872f988
buladou <kellyhu112@qq.com>
2017-03-01 22:54:28 +0800

    push

--- movie_spider/movie_spider/spiders/db_movie.py
+++ movie_spider/movie_spider/spiders/db_movie.py
@@ -8,14 +8,13 @@ class db_movie(scrapy.Spider):
 
     def parse_info(self, response):
         movie_info = MovieSpiderItem()
-        movie_info['title'] = response.xpath("//div[@id='content']/h1/span/text()").extract()
+        movie_info['name'] = response.xpath("//div[@id='content']/h1/span/text()").extract()
         movie_info['pic'] = response.xpath("//div[@id='mainpic']/a/img/@src").extract()
         movie_info['content'] = response.xpath("//div[@id='link-report']/span/text()").extract()
         movie_info['download'] = ""
-        return movie_info
+        print(movie_info)
 
     def parse(self, response):
         movie_links = response.xpath("//div[@id='showing-soon']/div/div/h3/a/@href").extract()
         for i in movie_links:
-            print(i)
-            # yield scrapy.Request(i, callback=parse_info)
+            yield scrapy.Request(i, callback=self.parse_info)
