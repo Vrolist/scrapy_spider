876ee8d57330110aaa118b4d35af017d16b8072e
布啦豆 <kellyhu112@qq.com>
2017-03-06 20:22:33 +0800

    push

--- practice/taobao_pc/taobao_pc/spiders/pc_spider.py
+++ practice/taobao_pc/taobao_pc/spiders/pc_spider.py
@@ -6,6 +6,5 @@ class taobao_pc(scrapy.Spider):
     start_urls = ["https://s.taobao.com/search?spm=a217h.7276445.1997814021.1.mDvDa7&app=vproduct&vlist=1&q=%E7%AC%94%E8%AE%B0%E6%9C%AC&cat=1101&from_type=3c"]
 
     def parse(self, response):
-        # print(response)
         sells = response.xpath("//div[@id='spulist-grid']/div[@class='m-grid']/div[@class='grid-container row']/div").extract()
         print(len(sells),type(sells))
